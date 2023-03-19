889266cb8ff2c35f6896b5ca253c200bd5d8e68c
dev-xpeace <xpeacedev@outlook.com>
2023-03-19 11:08:20 +0300

    exponentCalculator
    Basic exponent calculator.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(GetPow(4, 2));
+Console.ReadLine();
+
+static int GetPow(int baseNum, int powNum)
+{
+    int result = 1;
+
+    for(int i = 0; i < powNum; i++)
+    {
+        result = result * baseNum;
+    }
+    return result;
+
+
+}
